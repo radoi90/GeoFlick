a5831c84bc9707b10657f2ef7e89252946171214
Emil <eculic17@gmail.com>
2014-01-28 23:51:53 +0000

    Initial Commit

---
+++ GeoFlick/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoFlick
+//
+//  Created by Emil Culic on 28/01/14.
+//  Copyright (c) 2014 Emil Culic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GeoFlick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GeoFlick
+//
+//  Created by Emil Culic on 28/01/14.
+//  Copyright (c) 2014 Emil Culic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
